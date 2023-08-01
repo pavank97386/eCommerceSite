c73f7bfa50e2c178562a79bd42598b0892c5225d
pavan k <pavan.k@kaplan.edu>
2023-08-01 11:39:10 +0530

    delete user api added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,15 +1,32 @@
 const userSchema = require('../model/users')
 
-const getAllUserDetails = async (req,res)=>{
-        try{
-            const getAllUserData = await userSchema.find({},{name:1,age:1,role:1,_id:0})
-            return res.send(getAllUserData)
-             
-        }catch(err){
-            console.log("something went wrong while getting user admin side")
-        }
+const getAllUserDetails = async (req, res) => {
+    try {
+        const getAllUserData = await userSchema.find({}, { name: 1, age: 1, role: 1, _id: 0 })
+        return res.send(getAllUserData)
+
+    } catch (err) {
+        console.log("something went wrong while getting user admin side")
+    }
 }
 
+const deleteUserProfile = async (req, res) => {
+    try {
+        const { email } = req.body
+        const user = await userSchema.findOne({ email })
+        if (user !== null) {
+            const response = await userSchema.deleteOne({ email })
+            if (response.deletedCount > 0) {
+                return res.send("profile removed successfully")
+            }
+            return res.send("something went wronf while removing")
+        }
+        return res.send("user not found")
+    } catch (error) {
+        console.log("error while user deletion ", error);
+    }
+}
 module.exports = {
-    getAllUserDetails   
+    getAllUserDetails,
+    deleteUserProfile
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const adminController = require('../controllers/adminController')
 
 router.get('/',adminController.getAllUserDetails)
+router.delete('/',adminController.deleteUserProfile)
 
 
 module.exports = router;
